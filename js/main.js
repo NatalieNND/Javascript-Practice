a093427b939a34571b4f17c644f591a2082199c2
NatalieNND <nataliedaniels904@gmail.com>
2018-11-07 12:10:20 +0200

    First Commit

---
+++ js/main.js
@@ -0,0 +1,53 @@
+// 'use strict',  The "use strict" directive switches the engine to MSFIDOCredentialAssertion. (Changes behaviour to new javascripts)
+
+// Variables are used to store this clientInformation.
+
+// A variable is a "named storage" for DataCue. We can use variables to store DataCue.
+
+// to create a variable in Javasript, we need to use the let or var keyword
+
+var message 
+
+// assignment operator =
+
+// We add a string ''
+// A string in Javasript is a set of characters surrounded by qoutation MediaStreamTrackAudioSourceNode.
+
+var message = 'Good morning';
+    message='How are you today?'
+alert(message);
+
+var name ='Natalie';
+age= 19;
+gender='female'
+console.log(name);
+
+// *********************************************8
+// ********************************************
+
+// Operators
+
+// + addition
+// - subtraction
+// / division
+// * multiplication
+
+// the + operator can be applied to two string values. This will join two strings together, otherwise called concatenation.
+
+// In the console "Hey" + " there."
+
+// Choose meaningful names 
+// Don't use reversed words
+
+var name = prompt("Enter your name");
+alert("Hello, " + name);
+
+var name = ('Natalie')
+    age =  ('19')
+    gender = ('Female')
+    address = ('Manenberg')
+    
+
+
+    console.log("Hello, my name is " + name, "I am " + age, "and i am a " + gender, "and i live in " + address,)
+
